4746e5e84507e6593bc4a9befce66555aef8b9e0
Angus <anguscheng389@gmail.com>
2021-06-28 11:14:22 +1000

    Added floating point functionality

--- scripts.js
+++ scripts.js
@@ -15,8 +15,8 @@ function divide(a, b) {
 }
 
 function operate(a, b, op) {
-	a = parseInt(a);
-	b = parseInt(b);
+	a = parseFloat(a);
+	b = parseFloat(b);
 	switch(op) {
 		case '+': 
 			return result = add(a, b);
@@ -34,59 +34,6 @@ function operate(a, b, op) {
 const output = document.querySelector('.output');
 const btns = Array.from(document.querySelectorAll('.grid > button'));
 
-function displayKey(key) {
-	switch(key) {
-		case '0':
-			output.textContent = '0';
-			break;
-		case '1':
-			output.textContent = '1';
-			break;
-		case '2':
-			output.textContent = '2';
-			break;
-		case '3':
-			output.textContent = '3';
-			break;
-		case '4':
-			output.textContent = '4';
-			break;
-		case '5':
-			output.textContent = '5';
-			break;
-		case '6':
-			output.textContent = '6';
-			break;
-		case '7':
-			output.textContent = '7';
-			break;
-		case '8':
-			output.textContent = '8';
-			break;
-		case '9':
-			output.textContent = '9';
-			break;
-		case '+':
-			output.textContent = '+';
-			break;
-		case '-':
-			output.textContent = '-';
-			break;
-		case 'x':
-			output.textContent = 'x';
-			break;
-		case '/':
-			output.textContent = '/';
-			break;
-		case '=':
-			output.textContent = '=';
-			break;
-		case 'AC':
-			output.textContent = '0';
-			break;
-	}
-}
-
 function isNumeric(char) {
 	return (/([0-9]|\.)/.test(char));
 }
@@ -95,7 +42,6 @@ let statement = [0];
 let prevKey;
 
 function btnHandler (event) {
-	console.log(prevKey);
 	let key = event.target.textContent;
 	if (key === '=' && statement.length < 3) {
 		output.textContent = statement[0];
@@ -114,13 +60,11 @@ function btnHandler (event) {
 	} else if (!isNumeric(key)) {
 		statement.push(key);
 		output.textContent = key;
-		console.log('operand');
 
 	// Check if last keypress was an operand
 	} else if (!isNumeric(statement[statement.length-1])) {
 		statement.push(key);
 		output.textContent = key;
-		console.log('new num');
 
 	// Check if first number is 0
 	}  else if (statement[0] === 0) {
@@ -141,7 +85,6 @@ function btnHandler (event) {
 	} else {
 		statement[statement.length-1] += key; 
 		output.textContent += key;
-		console.log('prev num');
 	}
 
 	prevKey = key;
